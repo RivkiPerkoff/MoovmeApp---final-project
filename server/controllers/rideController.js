88a37b76dc8c26e2248887d13ac9179ffd9f61f2
RivkiPerkoff <Hand4d24@mbjcomp.org.il>
2025-06-09 15:50:45 +0300

    הוספת client ועדכון קבצי שרת

--- server/controllers/rideController.js
+++ server/controllers/rideController.js
@@ -50,10 +50,6 @@ const createRide = async (req, res) => {
   }
 };
 
-
-
-
-
 // Update ride
 const updateRide = async (req, res) => {
   try {
--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 app.use(express.json());
 
 // 1. Connect to MongoDB
